61546cbbd1a98217959176f7d38d1c14a865bcdd
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-01-10 02:59:55 +0200

    fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,14 +13,13 @@ def canUnlockAll(boxes):
     if len(boxes) == 1:
         return True
 
-    keys = boxes[0].copy()
-    count = 0
+    keys = list(boxes[0])
+    unlocked = [0]
 
-    for key in keys:
-        if boxes[key] == []:
-            count += 1
-        for new_key in boxes[key]:
-            if new_key not in keys and new_key < len(boxes):
-                keys.append(new_key)
+    while keys:
+        key = keys.pop()
+        if key not in unlocked and key < len(boxes):
+            unlocked.append(key)
+            keys.extend(boxes[key])
 
-    return len(keys)+count == len(boxes)
+    return len(unlocked) == len(boxes)
