6fbe20d42ffc3f8c2ac6fcecc4147494bf0a942d
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-01-24 14:05:19 +0200

    fixing raise exception

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,37 +6,36 @@ import sys
 import re
 
 
-if __name__ == "__main__":
-    """ script that reads stdin line by line and computes metrics"""
-    counter = 0
-    size = 0
-    status_code = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-                   "404": 0, "405": 0, "500": 0}
+counter = 0
+size = 0
+status_code = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
+interrupt_flag = False
 
-    try:
-        for line in sys.stdin:
-            match = re.match(
-                r"(.*) - \[.*\] \"GET /projects/260 HTTP/1.1\" (\d+) (.*)",
-                line)
-            if match:
-                counter += 1
-                size += int(match.group(3))
-                if match.group(2) in status_code:
-                    status_code[match.group(2)] += 1
+try:
+    for line in sys.stdin:
+        match = re.match(
+            r"(.*) - \[.*\] \"GET /projects/260 HTTP/1.1\" (\d+) (.*)",
+            line)
+        if match:
+            counter += 1
+            size += int(match.group(3))
+            if match.group(2) in status_code:
+                status_code[match.group(2)] += 1
 
-            if counter == 10:
-                print("File size: {}".format(size))
-                for key, value in sorted(status_code.items()):
-                    if value != 0:
-                        print("{}: {}".format(key, value))
-                counter = 0
-                size = 0
-                for key, value in sorted(status_code.items()):
-                    status_code[key] = 0
+        if counter == 10:
+            print("File size: {}".format(size))
+            for key, value in sorted(status_code.items()):
+                if value != 0:
+                    print("{}: {}".format(key, value))
+            counter = 0
+            size = 0
+            for key, value in sorted(status_code.items()):
+                status_code[key] = 0
 
-    except KeyboardInterrupt:
-        print("File size: {}".format(size))
-        for key, value in sorted(status_code.items()):
-            if value != 0:
-                print("{}: {}".format(key, value))
-        raise
+finally:
+    interrupt_flag = True
+    print("File size: {}".format(size))
+    for key, value in sorted(status_code.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
