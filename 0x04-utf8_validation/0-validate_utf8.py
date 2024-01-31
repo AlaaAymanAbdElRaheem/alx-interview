8626809cbc8e1a15d9c7ba45344fad8ce0178ccb
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-01-31 15:55:49 +0200

    fixing validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,22 +8,25 @@ def validUTF8(data: list) -> bool:
     else return False"""
     if not data:
         return False
+
+    bytes_list = []
     for i in data:
         if not isinstance(i, int):
             return False
         binary = format(i, '08b')
         if len(binary) > 8 or len(binary) < 1:
             return False
-        if binary.startswith('10'):
-            return False
         if binary.startswith('0'):
             continue
-        if binary.startswith('110'):
-            bytes = 1
+        if binary.startswith('10'):
+            bytes_list.append(1)
+        elif binary.startswith('110'):
+            bytes_list.append(2)
         elif binary.startswith('1110'):
-            bytes = 2
+            bytes_list.append(3)
         elif binary.startswith('11110'):
-            bytes = 3
+            bytes_list.append(4)
         else:
             return False
-    return True
+
+    return sum(bytes_list) <= 4
