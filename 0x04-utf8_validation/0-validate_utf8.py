480017ff3bdf86b02bd3574bfd6f21d244ee0986
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-01-31 14:43:18 +0200

    done

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" method that determines if a given data set
+represents a valid UTF-8 encoding"""
+
+
+def validUTF8(data: list) -> bool:
+    """return True if data is a valid UTF-8 encoding,
+    else return False"""
+    if not data:
+        return False
+    for i in data:
+        if i > 255:
+            return False
+    return True
