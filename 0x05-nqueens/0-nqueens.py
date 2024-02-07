d78c9c1ef5a29c1b08328b84ca25189e924ec842
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-02-07 23:48:59 +0200

    solved

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -50,19 +50,18 @@ def n_queens(borad: list, n: int, col: int, result: list) -> None:
 
 
 if __name__ == "__main__":
-    # """main function"""
-    # if len(sys.argv) != 2:
-    #     print("Usage: nqueens N")
-    #     sys.exit(1)
-    # if not sys.argv[1].isdigit():
-    #     print("N must be a number")
-    #     sys.exit(1)
-    # elif int(sys.argv[1]) < 4:
-    #     print("N must be at least 4")
-    #     sys.exit(1)
+    """main function"""
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    elif int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-    # n = int(sys.argv[1])
-    n = 4
+    n = int(sys.argv[1])
     board = [[0 for i in range(n)] for j in range(n)]
     result = []
     n_queens(board, n, 0, result)
